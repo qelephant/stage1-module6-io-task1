8ee7e42641adef544f785cda596858483e847dc5
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-03-16 09:28:29 +0600

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -6,6 +6,29 @@ import java.io.File;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String name = "";
+        int age = 0;
+        String email = "";
+        String phone = "";
+
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line = reader.readLine();
+        try {
+            while (line != null) {
+                if (line.startsWith("Name:")) {
+                    name = line.substring(5).trim();
+                } else if (line.startsWith("Age:")) {
+                    age = Integer.parseInt(line.substring(4).trim());
+                } else if (line.startsWith("Email:")) {
+                    email = line.substring(6).trim();
+                } else if (line.startsWith("Phone:")) {
+                    phone = line.substring(6).trim();
+                }
+                line = reader.readLine();
+            }
+        } finally {
+            reader.close();
+        }
+        return new Profile(name, age, email, phine);
     }
 }
