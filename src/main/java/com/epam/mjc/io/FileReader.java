338a56b0eb69d02ca3531b72b55061f20d97be97
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-03-16 17:24:39 +0600

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,8 +1,7 @@
 package com.epam.mjc.io;
 
 import java.io.File;
-import java.io.BufferedReader;
-import java.util.Scanner;
+import java.io.*;
 
 public class FileReader {
 
@@ -11,14 +10,15 @@ public class FileReader {
         int age = 0;
         String email = "";
         Long phone = 0L;
-
+        
         try {
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
+            FileInputStream fis = new FileInputStream(file);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+            while ((line = br.readLine()) != null) {
                 if (line.startsWith("Name:")) {
                     name = line.substring(5).trim();
-                    System.out.println(name);
                 } else if (line.startsWith("Age:")) {
                     age = Integer.parseInt(line.substring(4).trim());
                 } else if (line.startsWith("Email:")) {
@@ -26,10 +26,9 @@ public class FileReader {
                 } else if (line.startsWith("Phone:")) {
                     phone = Long.parseLong(line.substring(6).trim());
                 }
-
             }
-            scanner.close();
-        } catch (Exception e) {
+            br.close();
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
         return new Profile(name, age, email, phone);
