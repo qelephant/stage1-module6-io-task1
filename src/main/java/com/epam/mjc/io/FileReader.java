02586e5363ead84eba88017eb428d84355cd8816
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-12-12 10:56:28 +0600

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,39 +1,38 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.*;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file){
-        String name = "";
-        int age = 0;
-        String email = "";
-        Long phone = 0L;       
-        try {
-            FileInputStream fis = new FileInputStream(file);
-            InputStreamReader isr = new InputStreamReader(fis);
-            BufferedReader br = new BufferedReader(isr);
+    public Profile getDataFromFile(File file) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+
+            StringBuilder fileContent = new StringBuilder();
             String line;
-            while ((line = br.readLine()) != null) {
-                if (line.startsWith("Name:")) {
-                    name = line.substring(5).trim();
-                } else if (line.startsWith("Age:")) {
-                    age = Integer.parseInt(line.substring(4).trim());
-                } else if (line.startsWith("Email:")) {
-                    email = line.substring(6).trim();
-                } else if (line.startsWith("Phone:")) {
-                    phone = Long.parseLong(line.substring(6).trim());
+            while ((line = reader.readLine()) != null) {
+                fileContent.append(line).append("\n");
+            }
+            String[] lines = fileContent.toString().split("\n");
+            String name = null, age = null, email = null, phone = null;
+            for (String fileLine : lines) {
+                if (fileLine.startsWith("Name:")) {
+                    name = fileLine.substring("Name:".length()).trim();
+                } else if (fileLine.startsWith("Age:")) {
+                    age = fileLine.substring("Age:".length()).trim();
+                } else if (fileLine.startsWith("Email:")) {
+                    email = fileLine.substring("Email:".length()).trim();
+                } else if (fileLine.startsWith("Phone:")) {
+                    phone = fileLine.substring("Phone:".length()).trim();
                 }
             }
-        } catch (FileNotFoundException e){
+            return new Profile(name, Integer.parseInt(age), email, phone);
+
+        } catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
-        catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            br.close();
-        }
-        return new Profile(name, age, email, phone);
     }
 }
